84c7a361958dbd3b8b51f3a5b3a03da12727d473
Peter-Frank Spierenburg <spierepf@hotmail.com>
2016-08-28 12:44:29 -0300

    Fixed vendor lightup pop bumpers

--- modes/vendors/code/vendors.py
+++ modes/vendors/code/vendors.py
@@ -1,9 +1,69 @@
 from mpf.system.mode import Mode
+from mpf.system.timing import Timer
+
+class VendorBumper:
+    def __init__(self, machine, variable_name, hit_color, flash_color, leds):
+        self.machine = machine
+        self.variable_name = variable_name
+        self.hit_color = hit_color
+        self.flash_color = flash_color
+        self.leds = leds
+        self.on = False
+        self.reset()
+
+    def reset(self):
+        for led in self.leds:
+            led.color([0,0,0])
+
+    def tick(self):
+        player = self.machine.game.player
+        count = len(self.leds) - player[self.variable_name]
+
+        for i in range(0, count):
+            self.leds[i].color(self.hit_color)
+
+        self.on = not self.on
+        for i in range(count, len(self.leds)):
+            if(self.on):
+                self.leds[i].color(self.flash_color)
+            else:
+            	self.leds[i].color([0, 0, 0])
 
 class Vendors(Mode):
-
     def mode_init(self):
-        self.bottom_bumper = [
+        self.timer = Timer(callback=self.tick, frequency=1.0/5.0)
+
+        self.left_bumper = VendorBumper(self.machine, "vendor_left_hits_count", [0, 0, 255], [0, 0, 255/3], [
+                                self.machine.leds['l_vendor_left_a'],
+                                self.machine.leds['l_vendor_left_b'],
+                                self.machine.leds['l_vendor_left_c'],
+                                self.machine.leds['l_vendor_left_d'],
+                                self.machine.leds['l_vendor_left_e'],
+                                self.machine.leds['l_vendor_left_f'],
+                                self.machine.leds['l_vendor_left_g'],
+                                self.machine.leds['l_vendor_left_h'],
+                                self.machine.leds['l_vendor_left_i'],
+                                self.machine.leds['l_vendor_left_j'],
+                                self.machine.leds['l_vendor_left_k'],
+                                self.machine.leds['l_vendor_left_l'],
+                            ])
+
+        self.right_bumper = VendorBumper(self.machine, "vendor_right_hits_count", [0, 0, 255], [0, 0, 255/3], [
+                                self.machine.leds['l_vendor_right_a'],
+                                self.machine.leds['l_vendor_right_b'],
+                                self.machine.leds['l_vendor_right_c'],
+                                self.machine.leds['l_vendor_right_d'],
+                                self.machine.leds['l_vendor_right_e'],
+                                self.machine.leds['l_vendor_right_f'],
+                                self.machine.leds['l_vendor_right_g'],
+                                self.machine.leds['l_vendor_right_h'],
+                                self.machine.leds['l_vendor_right_i'],
+                                self.machine.leds['l_vendor_right_j'],
+                                self.machine.leds['l_vendor_right_k'],
+                                self.machine.leds['l_vendor_right_l'],
+                            ])
+
+        self.bottom_bumper = VendorBumper(self.machine, "vendor_bottom_hits_count", [0, 0, 255], [0, 0, 255/3], [
                                 self.machine.leds['l_vendor_bottom_a'],
                                 self.machine.leds['l_vendor_bottom_b'],
                                 self.machine.leds['l_vendor_bottom_c'],
@@ -16,25 +76,19 @@ class Vendors(Mode):
                                 self.machine.leds['l_vendor_bottom_j'],
                                 self.machine.leds['l_vendor_bottom_k'],
                                 self.machine.leds['l_vendor_bottom_l'],
-                            ]
+                            ])
+
 
     def mode_start(self, **kwargs):
-        self.machine.events.add_handler('timer_tick', self.tick)
-        for led in self.bottom_bumper:
-            led.color = [0,0,0]
+        self.machine.timing.add(self.timer)
 
     def mode_stop(self, **kwargs):
-        self.machine.events.remove_handler_by_event('timer_tick', self.tick)
-        for led in self.bottom_bumper:
-            led.color = [0,0,0]
+        self.machine.timing.remove(self.timer)
+        self.left_bumper.reset()
+        self.right_bumper.reset()
+        self.bottom_bumper.reset()
 
     def tick(self, **kwargs):
-        player = self.machine.game.player
-        for i in range(0, len(self.bottom_bumper) - player.vendor_bottom_hits_count):
-            self.bottom_bumper[i].color = [0, 0, 255]
-
-        for i in range(len(self.bottom_bumper) - player.vendor_bottom_hits_count, len(self.bottom_bumper)):
-            if(self.bottom_bumper[i].color == [0, 0, 0]):
-                self.bottom_bumper[i].color = [0, 0, 255]
-            else:
-            	self.bottom_bumper[i].color = [0, 0, 0]
+        self.left_bumper.tick()
+        self.right_bumper.tick()
+        self.bottom_bumper.tick()
