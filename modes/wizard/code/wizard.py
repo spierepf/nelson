6d3efe417f1abe187feadef0daf192cb94e7a2c8
Peter Spierenburg <peter.spierenburg@nautel.com>
2016-09-07 13:12:39 -0300

    Added shot groups to wizard mode

--- modes/wizard/code/wizard.py
+++ modes/wizard/code/wizard.py
@@ -2,7 +2,8 @@ from mpf.system.mode import Mode
 import random
 
 class WizardModeTarget:
-    def __init__(self, name, config):
+    def __init__(self, type_name, name, config):
+        self.type_name = type_name
         self.name = name
         self.config = config
 
@@ -12,14 +13,24 @@ class WizardModeTarget:
     def enable_event_name(self):
     	return self.config["enable_events"].items()[0][0]
 
+    def complete_event_name(self):
+        if self.type_name == "shot":
+            return self.name + "_wizard_active_hit"
+        else:
+            return self.name + "_wizard_hit_complete"
+
 class Wizard(Mode):
     def mode_init(self):
         self.wizard_mode_targets = []
         self.current_target = None
-        for shot_name in self.config["shots"]:
-            if any("wizard_mode_target" in s for s in self.config["shots"][shot_name]["tags"]):
-            	target = WizardModeTarget(shot_name, self.config["shots"][shot_name])
-                self.wizard_mode_targets += [target]
+
+        for type_name in ["shots", "shot_groups"]:
+            if type_name in self.config:
+                for name in self.config[type_name]:
+                    if any("wizard_mode_target" in s for s in self.config[type_name][name]["tags"]):
+                        target = WizardModeTarget(type_name, name, self.config[type_name][name])
+                        self.wizard_mode_targets += [target]
+
         self.log.info("Wizard Mode Targets: " + str(self.wizard_mode_targets))
 
     def mode_start(self):
@@ -41,7 +52,7 @@ class Wizard(Mode):
         self.disable_current_target()
         self.current_target = target
 
-        self.machine.events.add_handler(target.name + "_wizard_active_hit", self.target_hit)
+        self.machine.events.add_handler(target.complete_event_name(), self.target_hit)
 
         self.log.info("Enabling Wizard Mode Target: " + str(self.current_target))
         self.machine.events.post(self.current_target.enable_event_name())
